515c98dc3927f5999f7f1c1fd4b3fb54c1e52511
Patrick Martin <patrickmmartin@gmail.com>
2014-12-31 00:49:53 +0000

    adding bootstrap and simple usb test

---
+++ utils/lsusb.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import sys
+import usb.core
+# find all devices
+dev = usb.core.find(find_all=True)
+# loop through devices
+for cfg in dev:
+  sys.stdout.write(repr(cfg) + '\n') 
+# TODO(PMM) it would be nice to match the output of lsusb - needs sorting and description  
+#  sys.stdout.write('ID ' + hex(cfg.idVendor) + ' & ProductID=' + hex(cfg.idProduct) + '\n')
