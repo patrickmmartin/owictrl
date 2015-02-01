ab880ee3bace0b277300a3cbd8b98f50d4c25555
Patrick Martin <patrickmmartin@gmail.com>
2015-02-01 00:42:20 +0000

    adding simple command language test cases

--- utils/edgelang.py
+++ utils/edgelang.py
@@ -16,6 +16,37 @@ motor_map_bits = {
         }
 
 
+def parse_motor(command, res):
+    # Mn[+/-]
+    # get motor number
+    # get direction
+    clen = len(command)
+    lengths = set([2, 3])
+    if not (set([clen]) & lengths):
+        raise Exception('command not correct length %s (length %d)' % ("[2, 3]", clen))
+    m = int(command[1])
+    d = 1 if ((clen < 3) or (command[2] == '+')) else -1
+    res[m] = { 'dir' : d }
+    
+def parse_duration(command, res):
+    # Dn.nn
+    # get duration
+    d = float(command[1:])
+    res['D'] = 1.0
+    
+def parse_light(command, res):
+    # L[1/0]
+    # get on/off 
+    res['L'] = True
+   
+
+command_map = {        
+        'M'  : parse_motor,
+        'D'  : parse_duration,
+        'L'  : parse_light 
+        }
+
+
 """ semicolon delimited set of comma delimited set of params with a char code and 1-2 params """
 """ examples of input """
 """  'M1+,M2-,D1;M1-,M2+,D2' """
@@ -27,11 +58,17 @@ def to_ll(str):
     result = list()
     instructions = str.split(";")
     for instruction in instructions:
-        print (instruction)
         commands = instruction.split(",")
+        ret = {}
         for command in commands:
-            print("\t%s" % command)
-    
+            command = command.strip()
+            parse_func = command_map.get(command[0])
+            parse_func(command, ret)
+            # could enforce a duration here
+        result.append(ret)    
+        
+    return result
+
     # split on commas
 
 def to_hl(str):
--- utils/testarmlang.py
+++ utils/testarmlang.py
@@ -11,10 +11,21 @@ class EdgeLangBaseTestCase(unittest.TestCase):
     def setUp(self):
         pass
 
-class EdgeLangSimpleTestCase(EdgeLangBaseTestCase):
+class EdgeLangParseTestCase(EdgeLangBaseTestCase):
     def runTest(self):
-        instructions = edgelang.to_ll("M11,M21")
+        instructions = edgelang.to_ll("M1+,M2-,D1.0")
 
+class EdgeLangParseDefaultsTestCase(EdgeLangBaseTestCase):
+    def runTest(self):
+        instructions1 = edgelang.to_ll("M1,M2-,D1")
+        instructions2 = edgelang.to_ll("M1,M2-,D1")
+        self.assertEqual(instructions1[0], instructions2[0])
+
+class EdgeLangParseWSpaceDefaultsTestCase(EdgeLangBaseTestCase):
+    def runTest(self):
+        instructions1 = edgelang.to_ll("M1,M2-,D1")
+        instructions2 = edgelang.to_ll(" M1 , M2- , D1 ")
+        self.assertEqual(instructions1[0], instructions2[0])
 
 if __name__ == '__main__':
     unittest.main()
