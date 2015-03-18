2002ec52df6942f4d306adf969cd70d1c5991f36
Patrick Martin <patrickmmartin@gmail.com>
2015-03-18 23:20:52 +0000

    fix for the breakage in running checkers

--- utils/getch.py
+++ utils/getch.py
@@ -1,54 +1,39 @@
 # from recipe in http://code.activestate.com/recipes/134892-getch-like-unbuffered-character-reading-from-stdin/
 # there is a lot more in there, but I need to get that Mac to check it
 
-""" module to read chars live  """
-
-
-class _Getch(object):
-
+class _Getch:
     """Gets a single character from standard input.  Does not echo to the
 screen."""
-
     def __init__(self):
-        """ ctor """
         try:
             self.impl = _GetchWindows()
         except ImportError:
             self.impl = _GetchUnix()
 
-    def __call__(self):
-        """ returns result from delegate """
-        return self.impl()
+    def __call__(self): return self.impl()
 
 
-class _GetchUnix(object):
-
-    """ **nix style implementation """
+class _GetchUnix:
+    def __init__(self):
+        import tty, sys
 
     def __call__(self):
-        """ invocation of this implementation """
-        import sys
-        import tty
-        import termios
-        stdin_h = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(stdin_h)
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
         try:
-            tty.setraw(stdin_h)
-            char = stdin_h.read(1)
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
         finally:
-            termios.tcsetattr(stdin_h, termios.TCSADRAIN, old_settings)
-        return char
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
 
 
-class _GetchWindows(object):
-
-    """ windows implementation """
-
+class _GetchWindows:
     def __init__(self):
         import msvcrt
 
     def __call__(self):
-        """ invocation of this implementation """
         import msvcrt
         return msvcrt.getch()
 
